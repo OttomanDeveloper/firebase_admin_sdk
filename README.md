f74c8df01b93bb7f2ab732eb736d4b1626f30158
Muhammad Usman <ottomandeveloper@gmail.com>
2022-03-26 23:31:54 +0500

    First Release

---
+++ README.md
@@ -0,0 +1,7 @@
+
+A pure Dart implementation of the Firebase admin sdk
+
+Currently, only supports admin methods for the following firebase services:
+
+* authentication
+* realtime database
---
+++ example/main.dart
@@ -0,0 +1,34 @@
+import 'package:firebase_admin/firebase_admin.dart';
+
+void main() async {
+  // applicationDefault() will look for credentials in the following locations:
+  // * the service-account.json file in the package main directory
+  // * the env variable GOOGLE_APPLICATION_CREDENTIALS
+  // * a configuration file, specific for this library, stored in the user's home directory
+  // * gcloud's application default credentials
+  // * credentials from the firebase tools
+  var credential = Credentials.applicationDefault();
+
+  // when no credentials found, login using openid
+  // the credentials are stored on disk for later use
+  credential ??= await Credentials.login();
+
+  var projectId = 'some-project';
+  // create an app
+  var app = FirebaseAdmin.instance.initializeApp(AppOptions(
+      credential: credential,
+      projectId: projectId,
+      storageBucket: '$projectId.appspot.com'));
+
+  try {
+    // get a user by email
+    var v = await app.auth().getUserByEmail('jane@doe.com');
+    print(v.toJson());
+  } on FirebaseException catch (e) {
+    print(e.message);
+  }
+
+  await for (var v in app.storage().bucket().list()) {
+    print(v.name);
+  }
+}
